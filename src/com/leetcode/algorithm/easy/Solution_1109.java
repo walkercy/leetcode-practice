c771d131f6a5a48941694a796127b87dd9055ea7
walker <walkerc@qq.com>
2020-06-03 23:31:25 +0800

    Solution 1109

---
+++ src/com/leetcode/algorithm/easy/Solution_1109.java
@@ -0,0 +1,15 @@
+package com.leetcode.algorithm.easy;
+
+public class Solution_1109 {
+
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        int[] ans = new int[n];
+        for (int i = 0; i < bookings.length; i++) {
+            for (int j = bookings[i][0]; j <= bookings[i][1]; j++) {
+                ans[j - 1] += bookings[i][2];
+            }
+        }
+        return ans;
+    }
+
+}
